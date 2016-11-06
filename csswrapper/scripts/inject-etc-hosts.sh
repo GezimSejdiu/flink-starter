b9ee5c62d1852889dfed7c172576ea72b95a2920
gezim <g.sejdiu@gmail.com>
2016-11-06 02:40:26 +0100

    integrator-ui added

---
+++ csswrapper/scripts/inject-etc-hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "# flink-starter-data-integrator-ui" | sudo tee -a /etc/hosts
+echo "127.0.0.1  hdfs.demo.big-data-europe.local  hue.demo.big-data-europe.local flink-master.demo.big-data-europe.local flink-worker.demo.big-data-europe.local demo.big-data-europe.local monitor.demo.big-data-europe.local virtuoso.demo.big-data-europe.local" | sudo tee -a /etc/hosts;
+
+
